3fb20161fc76a809ad27e2ce93b977644346e424
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-06-09 11:06:59 +0530

    Main Push

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ if __name__ == '__main__':
     set_seed(seed_index)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     data = ImageDataset('data/monet_jpg', 'data/photo_jpg')
-    photo_data = PhotoDataset('/content/data/photo_jpg')
-    monet_data = PaintingDataset('/content/data/monet_jpg')
+    photo_data = PhotoDataset('data/photo_jpg')
+    monet_data = PaintingDataset('data/monet_jpg')
     data_loaded = load_data(data)
     monet_data_loaded = load_data(monet_data)
     photo_data_loaded = load_data(photo_data)
     show_example(data_loaded)
-    generator_real_to_painter, generator_painter_to_real = train(data_loaded, device = device)
+    generator_real_to_painter, generator_painter_to_real = train(data_loaded, device = device,
+                                                                 weight_path_if_any = "weights/cyclegan_17800.pth")
     get_submission(photo_data_loaded, generator = generator_real_to_painter)
